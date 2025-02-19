6e54e06bc89efe5f83dd7f44a01c7020627639f2
Kátia Cibele <katiacih@gmail.com>
2025-02-19 00:55:23 +0000

    add atributos dinamicos

--- playground-app/src/app/components/home/home.component.ts
+++ playground-app/src/app/components/home/home.component.ts
@@ -11,10 +11,18 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   myState: boolean = false;
+  texto: string = 'Texto';
+  ariaLabel: string = 'Aria Label';
 
 
   atualizarState(value: boolean) {
     this.myState = value;
   }
 
+  // método que será chamado ao clicar no botão
+  // o método irá atualizar o valor da variável myState
+  submit(event: Event) {
+    console.log('submit', event);
+    this.myState = !this.myState;
+  }
 }
